9cd4d7292cec65006acbbfd7ae3a425d581a63c8
Youssef Oufkir <youssef.oufkir.dev@gmail.com>
2022-01-05 21:52:10 +0100

    Sign-up Page UI implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import './App.css';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SignIn from './components/SignIn.js'
+import SignUp from './components/SignUp.js'
 
 function App() {
   var theme = createTheme({
@@ -15,57 +11,10 @@ function App() {
     },
   })
 
-  var styles = {
-    signUpButton: {
-      position: 'absolute', 
-      top: 0, 
-      right: 0, 
-      margin: '12px 25px 0 0',
-      color: '#365A0C'
-    },
-    
-    container: {
-      backgroundColor: '#FFFFFF',
-      height: '50vh',
-      width: '33vw',
-      minWidth: '350px',
-      margin: '12vh auto 0',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'space-around',
-      padding: '25px 0'
-    },
-
-    signInLabel: {
-      fontWeight: '600'
-    },
-
-    textField: {
-      width: '70%',
-    },
-
-    signInButton: {
-      width: '70%', 
-      padding: '9px 0', 
-      backgroundColor: '#365A0C', 
-      '&:hover': {
-        backgroundColor: '#538C12'
-      }
-    }
-  }
-
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-        <Button variant="text" sx={styles.signUpButton}>Sign up</Button>
-        <Paper elevation={2} sx={styles.container}>
-          <Typography variant='h4' sx={styles.signInLabel}>Sign In</Typography>
-          <TextField label="Email" variant="standard" color="primary" sx={styles.textField} />
-          <TextField label="Password" variant="standard" color="primary" sx={styles.textField} />
-          <Button variant="contained" sx={styles.signInButton}>Sign in</Button>
-        </Paper>
-      </Box>
+      {/* <SignIn />  */}
+      <SignUp /> 
     </ThemeProvider>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,63 @@
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+function SignUp() {
+    
+    var styles = {
+        SignInButton: {
+            color: '#365A0C',
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            margin: '12px 25px 0 0',
+        },
+        
+        container: {
+            width: '33vw', 
+            height: '50vh', 
+            backgroundColor: '#FFFFFF', 
+            margin: '12vh auto 0',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: '50px 0', 
+        },
+
+        signInLabel: {
+            fontWeight: '600'
+        },
+
+        textField: {
+            width: '70%'
+        },
+
+        signInButton: {
+            width: '70%', 
+            padding: '9px 0', 
+            backgroundColor: '#365A0C', 
+            '&:hover': {
+                backgroundColor: '#538C12'
+            },
+            margin: '9px 0 0'
+        }
+    }
+    
+    return ( 
+        <Box>
+            <Button variant="text" sx={styles.SignInButton}>Sign In</Button>
+            <Paper sx={styles.container}>
+                <Typography variant="h4" sx={styles.signInLabel}>Sign Up</Typography>
+                <TextField label="Name" variant="standard" color="primary" sx={styles.textField} />
+                <TextField label="Email" variant="standard" color="primary" sx={styles.textField} />
+                <TextField label="Password" variant="standard" color="primary" sx={styles.textField} />
+                <Button variant="contained" sx={styles.signInButton}>Sign Up</Button>
+            </Paper>
+        </Box>
+    );
+}
+
+export default SignUp;
